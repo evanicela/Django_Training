bd83c8258b87de5f8a6d90105c52046ff7bd649b
evanicela <niyonizigiye.celestin@gmail.com>
2024-07-01 22:04:40 +0300

    "Django Training"

---
+++ Training/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("contact/", views.contact, name="contacts"),
+    path("about-us/", views.about, name="about"),
+
+    path("service/", views.service, name="services"),
+
+
+    # path("courses/", views.courses, name="courses"),
+    # path("get-subjects/", views.get_subjects, name="get_subjects"),
+    # path("login/", views.login, name="login"),
+    # path("register/", views.register, name="register"),
+    # path("logout/", views.logout, name="logout"),
+
+]
